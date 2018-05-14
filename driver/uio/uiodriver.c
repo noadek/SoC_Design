d089432d8a31f24b89a38501202965caf36dd9bf
adadek <adadek@ttu.ee>
2018-05-14 19:11:03 +0300

    begin complete driver

--- driver/uio/uiodriver.c
+++ driver/uio/uiodriver.c
@@ -14,58 +14,128 @@
 #include "uiodriver.h"
 
 volatile int programError = 0;
-//get architecture specific page size
 unsigned pageSize;
+// get register variables
+void *networkAudio;
+void *filterLineIn;
+void *filterNetwork;
+void *volumeLineIn;
+void *volumeNetwork;
+int fd;
 
 int main(int argc, char *argv[]){
-	printf("Ready to receive...\n");
-	
-	// get register variables
-	void* ptr = get_virtual_address(UIO_AXI_AUDIO);
-	
-    // create data pipe
-    char* audioPipe = "/tmp/audiopipe";
-    int ret = mkfifo(audioPipe, 0666);
-    if ((ret == -1) && (errno != EEXIST)) {
-        perror("Error creating the named pipe");
-        return -1;
+	if (*argv[1] == 'p') {
+        printf("::::START_USAGE::::\n");
+        printf("FILTER EXAMPLE (high, band, low): %s f 1 0 0 \n", argv[0]);
+        printf("VOLUME EXAMPLE (left, right): %s v 4 -2 \n", argv[0]);
+		printf("Values should be multiples of 2.\n");
+        printf("::::END_USAGE::::\n");
     }
-    
-    // open pipe in read and write mode
-    int readFd = open(audioPipe, O_RDONLY|O_NONBLOCK);
-    int writeFd = open(audioPipe, O_WRONLY|O_NONBLOCK);
+    else {
+		printf("Ready to receive...\n");
+		
+		//get architecture specific page size
+		pageSize = sysconf(_SC_PAGESIZE);
 
-    // Create receiver thread
-    pthread_t receiverThread;
-    int err1 = pthread_create( &receiverThread, NULL, receive_audio, (void*) &writeFd);
-    if (err1) {
-        fprintf(stderr, "Error - pthread_create() return code: %d\n", err1);
-        return -1;
-    }
-    
-    //Read audio data from the FIFO pipe and put data into audio register
-    int16_t audioChunk;
-    while (1) {
-		if (programError) {
-			break;
+		// assign virtual addresses
+		GET_VIRTUAL_ADDRESS(UIO_FILTER_LINE_IN, filterLineIn);
+		GET_VIRTUAL_ADDRESS(UIO_FILTER_NETWORK, filterNetwork);
+		GET_VIRTUAL_ADDRESS(UIO_VOLUME_LINE_IN, volumeLineIn);
+		GET_VIRTUAL_ADDRESS(UIO_VOLUME_NETWORK, volumeNetwork);
+		GET_VIRTUAL_ADDRESS(UIO_AXI_AUDIO, networkAudio);
+		
+		FILTER_NETWORK_REG_0 = FILTER_LINE_REG_0 = 0x00002CB6;
+		FILTER_NETWORK_REG_1 = FILTER_LINE_REG_1 = 0x0000596C;
+		FILTER_NETWORK_REG_2 = FILTER_LINE_REG_2 = 0x00002CB6;
+		FILTER_NETWORK_REG_3 = FILTER_LINE_REG_3 = 0x8097A63A;
+		FILTER_NETWORK_REG_4 = FILTER_LINE_REG_4 = 0x3F690C9D;
+		FILTER_NETWORK_REG_5 = FILTER_LINE_REG_5 = 0x074D9236;
+		FILTER_NETWORK_REG_6 = FILTER_LINE_REG_6 = 0x00000000;
+		FILTER_NETWORK_REG_7 = FILTER_LINE_REG_7 = 0xF8B26DCA;
+		FILTER_NETWORK_REG_8 = FILTER_LINE_REG_8 = 0x9464B81B;
+		FILTER_NETWORK_REG_9 = FILTER_LINE_REG_9 = 0x3164DB93;
+		FILTER_NETWORK_REG_10 = FILTER_LINE_REG_10 = 0x12BEC333;
+		FILTER_NETWORK_REG_11 = FILTER_LINE_REG_11 = 0xDA82799A;
+		FILTER_NETWORK_REG_12 = FILTER_LINE_REG_12 = 0x12BEC333;
+		FILTER_NETWORK_REG_13 = FILTER_LINE_REG_13 = 0x00000000;
+		FILTER_NETWORK_REG_14 = FILTER_LINE_REG_14 = 0x0AFB0CCC;
+		
+		// reset and trigger filters
+		FILTER_LINE_RESET = 0;
+		FILTER_LINE_TRIGGER = 1;
+		
+		FILTER_NETWORK_RESET = 0;
+		FILTER_NETWORK_TRIGGER = 1;
+		
+		// set the default filters
+		FILTER_LINE_HIGH = 0;
+		FILTER_LINE_BAND = 0;
+		FILTER_LINE_LOW = 0;
+		
+		FILTER_NETWORK_HIGH = 0;
+		FILTER_NETWORK_BAND = 0;
+		FILTER_NETWORK_LOW = 0;
+		
+		// set default volume
+		VOLUME_LINE_1 = 256;
+		VOLUME_LINE_2 = 256;
+		
+		VOLUME_NETWORK_1 = 1024;
+		VOLUME_NETWORK_2 = 1024;
+		
+		// create data pipe
+		char* audioPipe = "/tmp/audiopipe";
+		int ret = mkfifo(audioPipe, 0666);
+		if ((ret == -1) && (errno != EEXIST)) {
+			perror("Error creating the named pipe");
+			return -1;
 		}
-        int bytes_read = read(readFd, &audioChunk, sizeof(int16_t));
-        if (bytes_read < 0) {
-            continue;
-        }
-        // sign extend 16 bit data and put in register
-        AUDIO_REGISTER = (int32_t) audioChunk;
-    }
-    close(readFd);
-    
-    // Wait till threads complete before main exits.
-    pthread_join(receiverThread, NULL);
-    
-    // remove the FIFO
-    unlink(audioPipe);
-    //delete mapping
-    munmap(ptr, pageSize);
+		
+		// open pipe in read and write mode
+		int readFd = open(audioPipe, O_RDONLY|O_NONBLOCK);
+		int writeFd = open(audioPipe, O_WRONLY|O_NONBLOCK);
 
+		// Create receiver thread
+		pthread_t receiverThread;
+		int err1 = pthread_create( &receiverThread, NULL, receive_audio, (void*) &writeFd);
+		if (err1) {
+			printf("Error - pthread_create() return code: %d\n", err1);
+			return -1;
+		}
+		
+		//Read audio data from the FIFO pipe and put data into audio register
+		int16_t audioChunk;
+		while (1) {
+			if (programError) {
+				printf("Exiting....\n");
+				break;
+			}
+			int bytes_read = read(readFd, &audioChunk, sizeof(int16_t));
+			if (bytes_read < 0) {
+				continue;
+			}
+			// sign extend 16 bit data and put in register
+			AUDIO_REGISTER = (int32_t) audioChunk;
+		}
+		close(readFd);
+		
+		// Wait till threads complete before main exits.
+		pthread_join(receiverThread, NULL);
+		
+		// remove the FIFO
+		unlink(audioPipe);
+		
+		//delete mapping
+		munmap(networkAudio, pageSize);
+		munmap(filterLineIn, pageSize);
+		munmap(filterNetwork, pageSize);
+		munmap(volumeLineIn, pageSize);
+		munmap(volumeNetwork, pageSize);
+		munmap(networkAudio, pageSize);
+		
+		printf("Closed!\n");
+	}
+	
     return 0;
 }
 
@@ -82,6 +152,7 @@ void *receive_audio(void* fd)
     
     while (1) {
 		if (programError || udp_client_recv(buffer, BYTES_RECEIVED) == 1) {
+			printf("Error occured while receiving.\n");
             break;
         }
         // write to FIFO
@@ -89,23 +160,4 @@ void *receive_audio(void* fd)
     }
     close(writeFd);
     pthread_exit(NULL);
-}
-
-/*
- * Map the physical address to a virtual address. 
- */
-void* get_virtual_address(const char* uio)
-{
-	pageSize = sysconf(_SC_PAGESIZE);
-	//open dev/uio#
-	int fd = open (uio, O_RDWR);
-	if (fd < 1) { 
-		perror("Error opening uio"); 
-		programError = 1; 
-	}
-	
-	//Redirect stdout/printf into /dev/kmsg file (so it will be printed using printk)
-	freopen ("/dev/kmsg","w",stdout);
-	
-	return mmap(NULL, pageSize, (PROT_READ, PROT_WRITE), MAP_SHARED, fd, pageSize * 0);
 }
--- driver/uio/uiodriver.h
+++ driver/uio/uiodriver.h
@@ -5,7 +5,6 @@
 #define PORT 				7891
 #define PACKET_SIZE        	512
 #define BYTES_RECEIVED 		1024
-#define AUDIO_REGISTER 		*((int32_t *)(ptr + 0))
 
 #define UIO_AXI_AUDIO		"/dev/uio0"
 #define UIO_FILTER_LINE_IN	"/dev/uio1"
@@ -14,15 +13,89 @@
 #define UIO_VOLUME_NETWORK	"/dev/uio4"
 #define UIO_OLED			"/dev/uio5"
 
+// AXI_AUDIO registers
+#define AUDIO_REGISTER 		*((int32_t *)(networkAudio + 0))
+
+// Filter line in registers
+
+// co-efficients  
+#define FILTER_LINE_REG_0   	*((unsigned *)(filterLineIn + 0))
+#define FILTER_LINE_REG_1   	*((unsigned *)(filterLineIn + 4))
+#define FILTER_LINE_REG_2   	*((unsigned *)(filterLineIn + 8))
+#define FILTER_LINE_REG_3   	*((unsigned *)(filterLineIn + 12)) 
+#define FILTER_LINE_REG_4   	*((unsigned *)(filterLineIn + 16))
+#define FILTER_LINE_REG_5   	*((unsigned *)(filterLineIn + 20))
+#define FILTER_LINE_REG_6   	*((unsigned *)(filterLineIn + 24)) 
+#define FILTER_LINE_REG_7   	*((unsigned *)(filterLineIn + 28))
+#define FILTER_LINE_REG_8   	*((unsigned *)(filterLineIn + 32))
+#define FILTER_LINE_REG_9   	*((unsigned *)(filterLineIn + 36)) 
+#define FILTER_LINE_REG_10  	*((unsigned *)(filterLineIn + 40))
+#define FILTER_LINE_REG_11  	*((unsigned *)(filterLineIn + 44))
+#define FILTER_LINE_REG_12  	*((unsigned *)(filterLineIn + 48)) 
+#define FILTER_LINE_REG_13  	*((unsigned *)(filterLineIn + 52))
+#define FILTER_LINE_REG_14  	*((unsigned *)(filterLineIn + 56))
+#define FILTER_LINE_RESET		*((unsigned *)(filterLineIn + 60))	
+#define FILTER_LINE_TRIGGER		*((unsigned *)(filterLineIn + 64))
+// 
+#define FILTER_LINE_HIGH		*((unsigned *)(filterLineIn + 68))
+#define FILTER_LINE_BAND		*((unsigned *)(filterLineIn + 72))
+#define FILTER_LINE_LOW			*((unsigned *)(filterLineIn + 76))
+
+// Filter network registers
+
+// co-efficients  
+#define FILTER_NETWORK_REG_0   	*((unsigned *)(filterNetwork + 0))
+#define FILTER_NETWORK_REG_1   	*((unsigned *)(filterNetwork + 4))
+#define FILTER_NETWORK_REG_2   	*((unsigned *)(filterNetwork + 8))
+#define FILTER_NETWORK_REG_3   	*((unsigned *)(filterNetwork + 12)) 
+#define FILTER_NETWORK_REG_4   	*((unsigned *)(filterNetwork + 16))
+#define FILTER_NETWORK_REG_5   	*((unsigned *)(filterNetwork + 20))
+#define FILTER_NETWORK_REG_6   	*((unsigned *)(filterNetwork + 24)) 
+#define FILTER_NETWORK_REG_7   	*((unsigned *)(filterNetwork + 28))
+#define FILTER_NETWORK_REG_8   	*((unsigned *)(filterNetwork + 32))
+#define FILTER_NETWORK_REG_9   	*((unsigned *)(filterNetwork + 36)) 
+#define FILTER_NETWORK_REG_10  	*((unsigned *)(filterNetwork + 40))
+#define FILTER_NETWORK_REG_11  	*((unsigned *)(filterNetwork + 44))
+#define FILTER_NETWORK_REG_12  	*((unsigned *)(filterNetwork + 48)) 
+#define FILTER_NETWORK_REG_13  	*((unsigned *)(filterNetwork + 52))
+#define FILTER_NETWORK_REG_14  	*((unsigned *)(filterNetwork + 56))
+#define FILTER_NETWORK_RESET	*((unsigned *)(filterNetwork + 60))	
+#define FILTER_NETWORK_TRIGGER	*((unsigned *)(filterNetwork + 64))
+//
+#define FILTER_NETWORK_RESET	*((unsigned *)(filterNetwork + 60))
+#define FILTER_NETWORK_TRIGGER	*((unsigned *)(filterNetwork + 64))
+//
+#define FILTER_NETWORK_HIGH		*((unsigned *)(filterNetwork + 68))
+#define FILTER_NETWORK_BAND		*((unsigned *)(filterNetwork + 72))
+#define FILTER_NETWORK_LOW		*((unsigned *)(filterNetwork + 76))
+
+
+// Volume line in registers
+#define VOLUME_LINE_1   		*((unsigned *)(volumeLineIn + 0))
+#define VOLUME_LINE_2   		*((unsigned *)(volumeLineIn + 4))
+
+// Volume line in registers
+#define VOLUME_NETWORK_1   		*((unsigned *)(volumeNetwork + 0))
+#define VOLUME_NETWORK_2   		*((unsigned *)(volumeNetwork + 4))
+
+
+/*
+ * Macro for mapping physical address to a virtual address. 
+ */
+#define GET_VIRTUAL_ADDRESS(uio, ptr)\
+	fd = open(uio, O_RDWR);\
+	if (fd < 1) \
+	{ \
+		printf("Error opening uio"); \
+		programError = 1; \
+	}\
+	ptr = mmap(NULL, pageSize, (PROT_READ, PROT_WRITE), \
+		MAP_SHARED, fd, pageSize * 0);\
+		
 /*
  * Receives data from the network and writes
  * to the FIFO pipe.
  */
 void* receive_audio(void*);
-
-/*
- * Map the physical address to a virtual address. 
- */
-void* get_virtual_address(const char*);
 
 #endif
